e32a236bdad50e3a714efc2c87e404cf7bd187e5
khouloud-kartal <114388142+khouloud-kartal@users.noreply.github.com>
2024-01-09 22:26:13 +0100

    job 5 is done

---
+++ Job05/index.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once './config.php';
+require_once './Product.php';
+require_once './Category.php';
+
+
+try{
+    $conn = getConnection();
+    if($conn !== null){
+        $request = 'SELECT * FROM product WHERE id = 7';
+        $request1 = $conn->prepare($request);
+        $request1->execute();
+        $result = $request1->fetch(PDO::FETCH_ASSOC);
+    }
+}catch(PDOException $e){
+    echo 'Error:' . $e->getMessage();
+}
+
+$product = new Product($result['id'], $result['name'], json_decode($result['photos']), $result['price'], $result['description'], $result['quantity'], new DateTime($result['createdAt']), new DateTime($result['updatedAt']), $result['categoryId']);
+
+var_dump($product->getCategory());
+
+?>
